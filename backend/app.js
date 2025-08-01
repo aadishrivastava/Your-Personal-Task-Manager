1291c5b94780b451f32b93d2199edaadbfbf943f
Aadi Shrivastava <aadishrivastava13@gmail.com>
2025-08-01 17:54:58 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express=require("express");
+require("dotenv").config();
+require("./conn/conn");
+const cors=require("cors");
+const bodyParser = require("body-parser");
+
+const app=express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit:"1mb"}));
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+app.use(cors({
+  origin:"http://localhost:3000",
+  Credential:true
+
+}));
+
+
+
+const UserAPI=require("./routes/user");
+const TaskAPI=require("./routes/task");
+app.use("/api/v1", UserAPI);  
+app.use("/api/v2", TaskAPI);  
+// app.get("/", (req, res) => {
+//   res.send("API is working ");
+// });
+
+// app.use("/",(req,res)=>{
+//     res.send("hello from backend side");
+// })
+const PORT=1000;
+app.listen(PORT,()=>{
+    console.log("Server is listening to PORT 1000");
+
+})
---
+++ backend/routes/user.js
@@ -0,0 +1,61 @@
+const router=require("express").Router();
+const User=require("../models/user");
+const bcrypt=require("bcryptjs");
+const jwt=require("jsonwebtoken");
+require("dotenv").config();
+
+router.post("/sign-in", async (req,res)=>{
+    console.log("BODY RECEIVED:", req.body);
+    try {
+        
+        const { username,email,password }=req.body;
+        const existingUser=await User.findOne({username:username});
+        const existingEmail=await User.findOne({email:email});
+        if(existingUser){
+            return res.status(400).json({message:"Username already exists"});
+        }else if(username.length<4){
+            return res.status(400).json({message:"Username should have atleast 4 characters"});
+        }
+        if(existingEmail){
+            return res.status(400).json({message:"Email already exists"});
+        }
+        
+        const hashpass=await bcrypt.hash(req.body.password,10);
+
+        const newUser=new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:hashpass,
+        });
+        await newUser.save();
+        return res.status(201).json({message:"sign up successfully"});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message:"Internal server error"});
+    }
+
+});
+
+router.post("/log-in",async (req,res)=>{
+    try {
+        const {username,password}=req.body;
+        const existingUser=await User.findOne({username:username});
+        if(!existingUser){
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+        bcrypt.compare(password,existingUser.password,(err,data)=>{
+            if(data){
+                const authClaims=[{name:username},{jti:jwt.sign({},process.env.JWT_SECRET)}]
+                const token=jwt.sign({authClaims},process.env.JWT_SECRET,{expiresIn:"2d"});
+                res.status(200).json({id:existingUser._id,token:token});
+            }else{
+                return res.status(400).json({message:"Inavlid Credentials"})
+            }
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message:"Internal server error"});
+    }
+})
+module.exports=router;
